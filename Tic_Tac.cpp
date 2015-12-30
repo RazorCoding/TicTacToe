3c6302cb18a9611280cbe47ff4efcb263d278ec8
Bushido <bursihido@gmail.com>
2015-12-30 16:22:42 +0530

    Create Tic_Tac.cpp

---
+++ Tic_Tac.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<vector>
+#include<cstdlib>
+std::vector<char> board = {'0','1','2','3','4','5','6','7','8','9'};
+
+class Tic{	
+	public:
+	int HumanMove(unsigned int pos, char X){
+		if(board[pos] == 'X' || board[pos] == 'Y'){
+			std::cout << "Postion is already filled" << std::endl;
+			return -1;
+		}
+		else{
+			board[char(pos)] = X;
+			std::cout << "You made your move"  << std::endl;
+			return 0;
+		}
+		
+	}
+	
+	void ComputerMove(char Y){
+		if(board[5] != '5'){
+			if((board[1] == '1' || board[2] == '2') && (board[3] == '3' || board[4] == '4') && (board[7] == '7' || board[4] == '4') && (board[8] == '8' || board[9] == '9')
+			&& (board[6] == '6' || board[4] == '4')){	
+				board[rand() % 9 ] = Y;
+				if( int T = board[rand() % 9 ] == '5'){
+					board[rand() % 9 ] = Y;
+				}
+			}
+		}
+		for(int i=1;i<10;i++){
+			if(board[i]-0x30 == i){
+				board[i] = Y;
+				break;
+			}
+			if(i==5)
+				i++;
+		}
+	}
+	void Board(){
+		std::cout << board[1] << "|" << board[2] << "|" << board[3] << std::endl;
+		std::cout << board[4] << "|" << board[5] << "|" << board[6] << std::endl;
+		std::cout << board[7] << "|" << board[8] << "|" << board[9] << std::endl;
+	}
+
+	void check()
+
+	void WinRows(int mark)
+	{
+		       char win[8][5] = {"0","1","2","3",
+					 "1","4","5","6",
+					 "2","7","8","9",
+	       				 "3","1","4","7",
+					 "4","2","5","8",
+	     				 "5","3","6","9",
+	 			         "6","1","5","9",																			 "7","3","5","7"
+				         "7","3","5","7"
+					};
+		if(mark == win[0][0])
+		{
+			std::cout << "First Match" << std::endl;
+		}
+		else if(mark == win[0][1])
+		{
+			std::cout << "Second Match" << std::endl;
+		}
+		else if(mark == win[0][2])
+		{
+			std::cout << "Thrid Match" << std::endl;
+			std::cout << "You Win "    << std::endl;
+		}	
+	}
+};	
+int main(){
+	char X = 'X';
+	char Y = 'Y';
+	for(int i;i<=5;i++){
+		Tic T;
+		T.Board();
+		std::cout << "On which postion: 1-9" << std::endl;
+		unsigned int pos;
+		std::cin >> pos;
+        	while(T.HumanMove(pos,X)){
+        		std::cout << "On which postion: 1-9" << std::endl;
+        		std::cin >> pos;
+		}
+		std::cout << std::endl;
+		T.Board();
+		T.ComputerMove(Y);
+		std::cout << std::endl;
+		T.Board();
+		std::cout << std::endl;
+	}
+	return 0;
+}
